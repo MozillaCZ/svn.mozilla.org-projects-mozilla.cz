59cc9015e64c1695cf7381fe98937b936d16b9be
Pavel Cvrček <jasnapaka@jasnapaka.com>
2010-01-10 23:03:29 +0000

    Oprava odkazů na produktové stránky.
    git-svn-id: https://svn.mozilla.org/projects/mozilla.cz@59479 4eb1ac78-321c-0410-a911-ec516a8615a5

--- www/wp-content/themes/mozillacz/products-header.php
+++ www/wp-content/themes/mozillacz/products-header.php
@@ -2,11 +2,11 @@
 				
 					<table>
 						<tr>
-							<td style="width:80px"><a href="/produkty/#firefox" title="Více informací o Mozilla Firefoxu">
+							<td style="width:80px"><a href="/produkty/firefox/" title="Více informací o Mozilla Firefoxu">
 									<img src="/images/firefox-64.png" width="64" height="64" alt="" /></a>
 							</td>
 							<td valign="top" >
-								<h3><a href="/produkty/#firefox" title="Více informací o Mozilla Firefoxu">Firefox 
+								<h3><a href="/produkty/firefox/" title="Více informací o Mozilla Firefoxu">Firefox 
 								<?php echo get_product_version('firefox') ?></a></h3>
 								<p>Moderní webový prohlížeč s řadou nadstandardních funkcí.</p>
 								<p class="products-header-download">
@@ -18,11 +18,11 @@
 							</td>
 						</tr>
 						<tr>
-							<td><a href="/produkty/#thunderbird" title="Více informací o Mozilla Thunderbirdu">
+							<td><a href="/produkty/thunderbird/" title="Více informací o Mozilla Thunderbirdu">
 									<img src="/images/thunderbird-64.png" width="64" height="64" alt="" /></a>
 							</td>
 							<td valign="top">
-								<h3><a href="/produkty/#thunderbird" title="Více informací o Mozilla Thunderbirdu">Thunderbird 
+								<h3><a href="/produkty/thunderbird/" title="Více informací o Mozilla Thunderbirdu">Thunderbird 
 								<?php echo get_product_version('thunderbird') ?></a></h3>
 								<p>Pokročilý poštovní klient pro snadnou správu vaší elektronické pošty.</p>
 								<p class="products-header-download">
