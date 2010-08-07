d1a8ce163b3961a7782c6136f53cebbb74be5917
Pavel Cvrček <jasnapaka@jasnapaka.com>
2010-08-07 16:10:41 +0000

    Bug 535387 - Vylepšit 404 stránku * upravení vzhledu stránky 404
    git-svn-id: https://svn.mozilla.org/projects/mozilla.cz@72029 4eb1ac78-321c-0410-a911-ec516a8615a5

--- www/wp-content/themes/mozillacz/404.php
+++ www/wp-content/themes/mozillacz/404.php
@@ -1,8 +1,21 @@
 <?php get_header(); ?>
 
-<h1>Stránka nebyla nalezena</h1>
+<h1>Ooops...</h1>
 
-<p>Stránka, na kterou jste přistoupili, nebyla bohužel na tomto serveru nalezena. Zkontrolujte, zda jste zadali adresu správně. Pro nalezení hledané stránky můžete též použít vyhledávání po pravé straně stránky.</p>
+<p>Zdá se, že stránka, kterou hledáte, nebyla na tomto serveru nalezena. Možná jste někde klepli
+na špatný odkaz či se uklepli při zadávání adresy do adresního řádku. Každopádně jsme vzbudili
+naši cvičenou pandu, aby to šla webmásterovi oznámit.</p>
+
+<p>Pokud by snad cestou opět usnula, dejte nám prosím o chybném odkazu vědět na <a 
+href="mailto:info@mozilla.cz">náš e-mail</a> a my se s tím pokusíme něco udělat. Nemusíte
+to též řešit a použít rovnou vyhledávání po pravé straně stránky.</p>
+
+<p><a href="http://commons.wikimedia.org/wiki/File:La_Palmyre_027.jpg">
+	<img src="<?php bloginfo('template_directory'); ?>/images/panda.jpg" alt="Spící panda" class="center" /></a>
+</p>
+<div style="text-align: right; font-size: 10px; margin-right: 40px; margin-top: -10px;">
+Autor: <a href="http://commons.wikimedia.org/wiki/User:Manchot">William Scot</a>, CC-BY-SA
+</div>
 
 <?php get_sidebar(); ?>
 
