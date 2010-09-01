03efd4958e0dbb1c994ecee4aee02fd84bee98e2
Pavel Cvrček <jasnapaka@jasnapaka.com>
2010-09-01 19:06:05 +0000

    Bug 592675 - Mikroformat pro licenci * aktualizace na f.m.c a t.m.c
    git-svn-id: https://svn.mozilla.org/projects/mozilla.cz@73685 4eb1ac78-321c-0410-a911-ec516a8615a5

--- thunderbird/inc/tpl/footer.php
+++ thunderbird/inc/tpl/footer.php
@@ -12,7 +12,8 @@
 	<p>
 		© 2004–2006 <a href="http://www.czilla.cz/czilla/lide.html">CZilla</a>
 		© 2009–2010 <a href="http://www.mozilla.cz/o-nas/">Mozilla.cz</a>
-		Obsah je dostupný pod licencí <a href="http://creativecommons.org/licenses/by-sa/3.0/deed.cs" title="Více informací o licenci">Creative Commons Attribution-Share Alike 3.0</a>
+		Obsah je dostupný pod licencí <a href="http://creativecommons.org/licenses/by-sa/3.0/deed.cs" 
+		title="Více informací o licenci" rel="license">Creative Commons Attribution-Share Alike 3.0</a>
 	</p>
 </div>
 </div>
