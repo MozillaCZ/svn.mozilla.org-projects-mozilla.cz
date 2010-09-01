da49e7d6f862e3a0c11590e92e3f4ac4045fc21c
Pavel Cvrček <jasnapaka@jasnapaka.com>
2010-09-01 19:03:01 +0000

    Bug 592675 - Mikroformat pro licenci * přidání mirkoformátu na Mozilla.cz
    git-svn-id: https://svn.mozilla.org/projects/mozilla.cz@73684 4eb1ac78-321c-0410-a911-ec516a8615a5

--- www/wp-content/themes/mozillacz/footer.php
+++ www/wp-content/themes/mozillacz/footer.php
@@ -5,7 +5,7 @@
 &copy; 2009&ndash;2010 <a href="/o-nas/" title="Více informací o nás">Mozilla.cz</a>,
 <a href="/feed/" title="Odběr novinek ve formátu RSS">RSS</a>,
  <a href="/mapa-webu/" title="Mapa webu">mapa webu</a>,
-Obsah je dostupný pod licencí <a href="http://creativecommons.org/licenses/by-sa/3.0/deed.cs" title="Více informací o licenci">Creative Commons Attribution-Share Alike 3.0</a>.
+Obsah je dostupný pod licencí <a href="http://creativecommons.org/licenses/by-sa/3.0/deed.cs" title="Více informací o licenci" rel="license">Creative Commons Attribution-Share Alike 3.0</a>.
 </div>
 	</div>
 
